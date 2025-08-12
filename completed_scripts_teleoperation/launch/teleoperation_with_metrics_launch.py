ca929a22d520ea874509b865f67f6a094dcb49b8
Alexander Grachev <sashagrachev2005@gmail.com>
2025-08-12 19:04:44 +0300

    Commit on 2025-08-12_19:04:44

--- completed_scripts_teleoperation/launch/teleoperation_with_metrics_launch.py
+++ completed_scripts_teleoperation/launch/teleoperation_with_metrics_launch.py
@@ -23,7 +23,10 @@ JOINTS_TO_CHECK = [12, 13, 14, 15, 23, 31]
 def generate_launch_description():
     return LaunchDescription(
         [
-            Node(package="repeater_package", executable="repeater_node"),
+            Node(
+                package="repeater_package",
+                executable="repeater_node",
+            ),
             Node(
                 package="converter_from_ukt_to_h1_package",
                 executable="converter_from_ukt_to_h1_node",
